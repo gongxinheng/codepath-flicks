25e37052889bf2808559dafae92bc27c17cbe639
Hengstar Gong <gongxinheng@gmail.com>
2017-09-14 23:05:18 -0700

    Initial version (followed walkthrough video)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # codepath-flicks
 A read-only movie listing app using the Movie Database API
+Initial version
---
+++ app/src/main/java/com/hengstar/flickster/models/Movie.java
@@ -0,0 +1,45 @@
+package com.hengstar.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Movie {
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+
+    private String posterPath;
+    private String originalTitle;
+    private String overView;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overView = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array) {
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int x = 0; x < array.length(); x++) {
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+}
